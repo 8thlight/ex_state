340c1183c81cf6338b1f7d2b0ba48df729aeaa1f
Kevin Buchanan <kevaustinbuch@gmail.com>
2020-04-29 14:39:57 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExState.MixProject do
   def project do
     [
       app: :ex_state,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
